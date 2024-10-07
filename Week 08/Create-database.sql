c8d6c61bdc5db1bc5a282fb44e3f1604ac3c10b0
zeken <49985689+zeken@users.noreply.github.com>
2024-10-07 14:14:28 +0800

    sql

--- Week 08/Create-database.sql
+++ Week 08/Create-database.sql
@@ -25,3 +25,13 @@ VALUES
 
 -- Verify that the data was inserted correctly
 SELECT * FROM products;
+-- Insert data into the orders table
+INSERT INTO orders (product_name, size, quantity, unit_price, total_price, order_date)
+VALUES
+('Just Java', 'Endless Cup', 10, 2.00, 20.00, '2024-10-04 14:53:36'),
+('Cafe au Lait', 'Double', 9, 3.00, 27.00, '2024-10-04 14:53:36'),
+('Cafe au Lait', 'Single', 1, 3.00, 3.00, '2024-10-04 14:55:39'),
+('Iced Cappuccino', 'Double', 10, 5.75, 57.50, '2024-10-04 15:27:50'),
+('Cafe au Lait', 'Double', 1, 3.00, 3.00, '2024-10-04 19:37:28'),
+('Iced Cappuccino', 'Double', 1, 5.75, 5.75, '2024-10-04 19:37:28'),
+('Iced Cappuccino', 'Double', 1, 5.75, 5.75, '2024-10-04 19:37:55');
